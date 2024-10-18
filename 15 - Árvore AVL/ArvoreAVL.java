621aaa4b2935656a6fa2f3c7b71e1d69adc09169
Antônio Junior <102259875+afcj8@users.noreply.github.com>
2024-10-18 17:19:12 +0000

    função para mostrar, calcular altura, profundidade e fator de balanceamento

--- 15 - Árvore AVL/ArvoreAVL.java
+++ 15 - Árvore AVL/ArvoreAVL.java
@@ -4,6 +4,7 @@ public class ArvoreAVL {
     private NoAVL raiz;
     Comparador c = new Comparador();
     private int tamanho;
+    private ArrayList<NoAVL> aux;
 
     public ArvoreAVL() {
         this.raiz = null;
@@ -57,49 +58,99 @@ public class ArvoreAVL {
         }
     }
 
+    public NoAVL incluir(Object key) {
+        NoAVL pai = pesquisar(raiz, key);
+        NoAVL novo = new NoAVL(pai, key);
+
+        if (pai == null) {
+            raiz = novo;
+            tamanho++;
+            novo.setFb(0);
+            return novo;
+        }
+
+        if (novo.getElement() == pai.getElement()) {
+            return pai;
+        }
+
+        if (c.compare(key, pai.getElement()) < 0) {
+            pai.setFilhoEsquerdo(novo);
+        } else {
+            pai.setFilhoDireito(novo);
+        }
+        
+        tamanho++;
+        return novo;
+    }
+
+
     public int height(NoAVL no) {
         if (no == null) {
-            return 0;
+            return -1;
         }
-        if (isExternal(no)) {
-            return 0;
+        int hEsquerda = height(no.getFilhoEsquerdo());
+        int hDireita = height(no.getFilhoDireito());
+        return Math.max(hEsquerda, hDireita) + 1; 
+    }
+
+    public int depth(NoAVL no) {
+        int profundidade = 0;
+        while (no != raiz) {
+            no = no.getPai();
+            profundidade++;
         }
-        int h = 0;
-        if (no.getFilhoEsquerdo() != null) {
-            h = Math.max(h, height(no.getFilhoEsquerdo()));
-        }
-        if (no.getFilhoDireito() != null) {
-            h = Math.max(h, height(no.getFilhoDireito()));
-        }
-        return 1 + h;
+        return profundidade;
     }
 
     public int fatorBalanceamento(NoAVL no) {
+        if (no == null) {
+            return 0;
+        }
         int hEsquerda = height(no.getFilhoEsquerdo());
         int hDireita = height(no.getFilhoDireito());
         return hEsquerda - hDireita;
     }
 
-    private NoAVL balancear(NoAVL no) {
-        int fb = fatorBalanceamento(no);
-
-        // Rotação simples à esquerda
-        if (fb < -1 && fatorBalanceamento(no.getFilhoDireito()) <= 0) {
-            no = rotacaoEsquerda(no);
-        }
-        // Rotação simples à direita
-        else if (fb > 1 && fatorBalanceamento(no.getFilhoEsquerdo()) >= 0) {
-            no = rotacaoDireita(no);
-        }
-        // Rotação dupla á esquerda
-        else if (fb > 1 && fatorBalanceamento(no.getFilhoEsquerdo()) < 0) {
-            no = rotacaoDuplaEsquerda(no);
+    public void mostrar() {
+        if (raiz == null) {
+            System.out.println("Árvore vazia");
+            return;
         } 
-        // Rotação dupla à direita
-        else if (fb < -1 && fatorBalanceamento(no.getFilhoDireito()) > 0) {
-            no = rotacaoDuplaDireita(no);
+
+        Object[][] matriz = new Object[height(raiz) + 1][tamanho];
+        aux = new ArrayList();
+        emOrdem(raiz);
+
+        for (int i = 0; i < aux.size(); i++) {
+            Object obj = ((NoAVL)aux.get(i)).getElement();
+            matriz[depth((NoAVL)aux.get(i))][i] = obj;
         }
 
-        return no;
+        for (int i = 0; i < height(raiz) + 1; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                int fb = fatorBalanceamento((NoAVL)aux.get(j));
+                if (matriz[i][j] == null) {
+                    System.out.print("\t");
+                } else {
+                    System.out.printf("\t%d" + "[" + fb + "]", matriz[i][j]);
+                }
+            }
+            System.out.println();
+        } 
+            
+    }
+
+    public void emOrdem(NoAVL no) {
+        if (isInternal(no)) {
+            if (no.getFilhoEsquerdo() != null) {
+                emOrdem(no.getFilhoEsquerdo());
+            }
+        }
+        aux.add(no); // visite
+        if (isInternal(no)) {
+            if (no.getFilhoDireito() != null) {
+                emOrdem(no.getFilhoDireito());
+            }
+        }
     }
 }
--- 15 - Árvore AVL/Teste.java
+++ 15 - Árvore AVL/Teste.java
@@ -1,10 +1,11 @@
 public class Teste {
     public static void main(String[] args) {
         ArvoreAVL avl = new ArvoreAVL(10);
-        System.out.println(avl.getRaiz().getElement());
-        System.out.println(avl.isRoot(avl.getRaiz()));
-        System.out.println(avl.isExternal(avl.getRaiz()));
-        System.out.println(avl.isInternal(avl.getRaiz()));
-        System.out.println(avl.pesquisar(avl.getRaiz(), 10));
+        avl.incluir(15);
+        avl.incluir(20);
+        // avl.incluir(25);
+        System.out.println(avl.height(avl.getRaiz()));
+        System.out.println(avl.fatorBalanceamento(avl.getRaiz()));
+        avl.mostrar();
     }
 }

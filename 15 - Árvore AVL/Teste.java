c8f600a5e8714c6ab9ebaaf5d47a49f6f05cd7fe
Antônio Junior <102259875+afcj8@users.noreply.github.com>
2024-10-19 00:36:22 +0000

    inserção e rotações simples funcionando

--- 15 - Árvore AVL/Teste.java
+++ 15 - Árvore AVL/Teste.java
@@ -1,18 +1,25 @@
 public class Teste {
     public static void main(String[] args) {
-        ArvoreAVL avl = new ArvoreAVL(3);
-        NoAVL noA = avl.incluir(2);
-        NoAVL noB = avl.incluir(1);
+        ArvoreAVL avl = new ArvoreAVL(1);
         avl.mostrar();
-
+        NoAVL noA = avl.incluir(2);
+        avl.mostrar();
+        NoAVL noB = avl.incluir(3);
+        avl.mostrar();
         NoAVL noC = avl.incluir(4);
+        avl.mostrar();
         NoAVL noD = avl.incluir(5);
         avl.mostrar();
-
         NoAVL noE = avl.incluir(6);
         avl.mostrar();
-
         NoAVL noF = avl.incluir(7);
         avl.mostrar();
+        NoAVL noG = avl.incluir(8);
+        avl.mostrar();
+        NoAVL noH = avl.incluir(9);
+        avl.mostrar();
+        NoAVL noI = avl.incluir(10);
+        avl.mostrar();
+
     }
 }

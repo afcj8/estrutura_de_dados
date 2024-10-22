4af6c372167a47c0fd7bbbfdf9bfa38e7ec352f6
Antônio Junior <102259875+afcj8@users.noreply.github.com>
2024-10-22 20:11:08 +0000

    rotações simples funcionando na inserção

--- 15 - Árvore AVL/Teste.java
+++ 15 - Árvore AVL/Teste.java
@@ -1,29 +1,31 @@
 public class Teste {
     public static void main(String[] args) {
-        ArvoreAVL avl = new ArvoreAVL(10);
-        NoAVL noA = avl.incluir(5);
+        ArvoreAVL avl = new ArvoreAVL(100);
+        NoAVL noA = avl.incluir(90);
         avl.mostrar();
-        NoAVL noB = avl.incluir(3);
+        NoAVL noB = avl.incluir(80);
         avl.mostrar();
-        // NoAVL noC = avl.incluir(12);
-        // avl.mostrar();
-        // NoAVL noD = avl.incluir(18);
-        // avl.mostrar();
+        NoAVL noC = avl.incluir(70);
+        avl.mostrar();
+        NoAVL noD = avl.incluir(60);
+        avl.mostrar();
 
         // avl.remover(10);
         // avl.mostrar();
         // avl.remover(5);
         // avl.mostrar();
-        // NoAVL noD = avl.incluir(40);
-        // avl.mostrar();
-        // NoAVL noE = avl.incluir(30);
-        // avl.mostrar();
-        // NoAVL noF = avl.incluir(4);
-        // avl.mostrar();
-        // NoAVL noG = avl.incluir(3);
-        // avl.mostrar();
-        // NoAVL noH = avl.incluir(2);
-        // avl.mostrar();
+
+        NoAVL noE = avl.incluir(50);
+        avl.mostrar();
+        NoAVL noF = avl.incluir(40);
+        avl.mostrar();
+        NoAVL noG = avl.incluir(30);
+        avl.mostrar();
+        NoAVL noH = avl.incluir(20);
+        avl.mostrar();
+        NoAVL noI = avl.incluir(10);
+        avl.mostrar();
+
         // NoAVL noI = avl.incluir(1);
         // avl.mostrar();
     }

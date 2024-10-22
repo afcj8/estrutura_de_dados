c29d9ad128aef8dd5fa7ba55ff87b122c545f7fd
Antônio Junior <102259875+afcj8@users.noreply.github.com>
2024-10-22 20:25:00 +0000

    avl finalizada

--- 15 - Árvore AVL/Teste.java
+++ 15 - Árvore AVL/Teste.java
@@ -1,32 +1,29 @@
 public class Teste {
     public static void main(String[] args) {
-        ArvoreAVL avl = new ArvoreAVL(100);
-        NoAVL noA = avl.incluir(90);
+        ArvoreAVL avl = new ArvoreAVL(40);
+        NoAVL noA = avl.incluir(20);
         avl.mostrar();
-        NoAVL noB = avl.incluir(80);
+        NoAVL noB = avl.incluir(50);
         avl.mostrar();
-        NoAVL noC = avl.incluir(70);
+        NoAVL noC = avl.incluir(60);
         avl.mostrar();
-        NoAVL noD = avl.incluir(60);
-        avl.mostrar();
-
-        // avl.remover(10);
+        // NoAVL noD = avl.incluir(90);
         // avl.mostrar();
+
+        avl.remover(20);
+        avl.mostrar();
         // avl.remover(5);
         // avl.mostrar();
 
-        NoAVL noE = avl.incluir(50);
-        avl.mostrar();
-        NoAVL noF = avl.incluir(40);
-        avl.mostrar();
-        NoAVL noG = avl.incluir(30);
-        avl.mostrar();
-        NoAVL noH = avl.incluir(20);
-        avl.mostrar();
-        NoAVL noI = avl.incluir(10);
-        avl.mostrar();
-
-        // NoAVL noI = avl.incluir(1);
+        // NoAVL noE = avl.incluir(60);
+        // avl.mostrar();
+        // NoAVL noF = avl.incluir(40);
+        // avl.mostrar();
+        // NoAVL noG = avl.incluir(30);
+        // avl.mostrar();
+        // NoAVL noH = avl.incluir(20);
+        // avl.mostrar();
+        // NoAVL noI = avl.incluir(10);
         // avl.mostrar();
     }
 }

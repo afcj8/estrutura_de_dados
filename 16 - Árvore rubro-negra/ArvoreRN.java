77af29597f49a03c04766feee97a23ab93f0c7fc
Antônio Junior <102259875+afcj8@users.noreply.github.com>
2024-10-23 16:55:41 +0000

    alguns métodos que são padrão

--- 16 - Árvore rubro-negra/ArvoreRN.java
+++ 16 - Árvore rubro-negra/ArvoreRN.java
@@ -6,9 +6,34 @@ public class ArvoreRN {
     private int tamanho;
     private ArrayList<NoRB> aux;
 
+    public ArvoreRN() {
+        this.raiz = null;
+        this.tamanho = 0;
+    }
+
     public ArvoreRN(Object obj) {
         this.raiz = new NoRB(null, obj);
         this.tamanho++;
         this.raiz.setCor("N");
+    }
+
+    public NoRB getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NoRB raiz) {
+        this.raiz = raiz;
+    }
+
+    public boolean isRoot(NoRB no) {
+        return no == this.raiz;
+    }
+
+    public boolean isExternal(NoRB no) {
+        return no.getFilhoDireito() == null && no.getFilhoEsquerdo() == null;
+    }
+
+    public boolean isInternal(NoRB no) {
+        return no.getFilhoDireito() != null || no.getFilhoEsquerdo() != null;
     }
 }

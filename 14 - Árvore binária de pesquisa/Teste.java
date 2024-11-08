79817fbb096cbe1bd40def04a47deaedf3468e84
Antônio Junior <102259875+afcj8@users.noreply.github.com>
2024-11-08 19:19:30 +0000

    testes realizados

--- 14 - Árvore binária de pesquisa/Teste.java
+++ 14 - Árvore binária de pesquisa/Teste.java
@@ -4,6 +4,7 @@ public class Teste {
     public static void main(String[] args) {
         ArvoreBinariaPesquisa abp = new ArvoreBinariaPesquisa(10);
         Comparador c = new Comparador();
+        
         No noA = abp.incluir(4);
         No noB = abp.incluir(8);
         No noC = abp.incluir(2);
@@ -13,21 +14,6 @@ public class Teste {
         No noI = abp.incluir(20);
         No noJ = abp.incluir(1);
         
-        System.out.println("---------------- Nós ---------------");
-        Iterator<No> x = abp.nos();
-        while (x.hasNext()) {
-            No aux = x.next();
-            System.out.print(aux + " ");
-        }
-
-        System.out.println();
-        System.out.println("------------- Elementos ------------");
-        Iterator<No> y = abp.elements();
-        while (y.hasNext()) {
-            No aux = y.next();
-            System.out.print(aux.getElement() + " ");
-        }
-        System.out.println();
         System.out.println("Profundidade: " + abp.depth(abp.getRaiz()));
         System.out.println("Altura      : " + abp.height(abp.getRaiz()));
         System.out.println();
@@ -57,16 +43,16 @@ public class Teste {
         abp.remover(13);
         abp.mostrar();
 
-        System.out.println("Removendo o 18: ");
-        abp.remover(18);
+        System.out.println("Removendo o 1: ");
+        abp.remover(1);
         abp.mostrar();
 
         System.out.println("Removendo o 20: ");
         abp.remover(20);
         abp.mostrar();
 
-        System.out.println("Removendo o 1: ");
-        abp.remover(1);
+        System.out.println("Removendo o 18: ");
+        abp.remover(18);
         abp.mostrar();
     }
 }

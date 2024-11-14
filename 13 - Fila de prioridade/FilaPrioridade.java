f0665634888257c6ffe955799a2f8ede321eefda
Antônio Junior <102259875+afcj8@users.noreply.github.com>
2024-11-14 12:52:14 +0000

    classe FilaPrioridade

---
+++ 13 - Fila de prioridade/FilaPrioridade.java
@@ -0,0 +1,59 @@
+public class FilaPrioridade {
+    private No inicio;
+    private No fim;
+    public FilaPrioridade() {
+        this.inicio = null;
+        this.fim = null;
+    }
+    public void enfileirar(Object elemento, int prioridade) {
+        No novo = new No(elemento, prioridade);
+        if (this.inicio == null) {
+            this.inicio = novo;
+            this.fim = novo;
+        } else {
+            No atual = this.inicio;
+            No anterior = null;
+            while (atual != null && atual.getPrioridade() >= prioridade) {
+                anterior = atual;
+                atual = atual.getProximo();
+            }
+            if (anterior == null) {
+                novo.setProximo(this.inicio);
+                this.inicio = novo;
+            } else {
+                anterior.setProximo(novo);
+                novo.setProximo(atual);
+            }
+            if (atual == null) {
+                this.fim = novo;
+            }
+        }
+    }
+    public Object desenfileirar() throws FilaPrioridadeException {
+        if (this.inicio == null) {
+            throw new FilaPrioridadeException("Fila vazia");
+        }
+        Object elemento = this.inicio.getElemento();
+        this.inicio = this.inicio.getProximo();
+        if (this.inicio == null) {
+            this.fim = null;
+        }
+        return elemento;
+    }
+    public Object inicio() throws FilaPrioridadeException {
+        if (this.inicio == null) {
+            throw new FilaPrioridadeException("Fila vazia");
+        }
+        return this.inicio.getElemento();
+    }
+    public boolean vazia() {
+        return this.inicio == null;
+    }
+    public void imprimir() {
+        No atual = this.inicio;
+        while (atual != null) {
+            System.out.println(atual.getElemento() + " - " + atual.getPrioridade());
+            atual = atual.getProximo();
+        }
+    }
+}
